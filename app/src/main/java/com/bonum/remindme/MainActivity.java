c51db0ecc93739bc0344a713159c6cc092368149
sheidon <ozaHIP78>
2017-02-03 03:31:13 +0700

    Create MainActivity class and Layout

---
+++ app/src/main/java/com/bonum/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bonum.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 02.02.2017.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
