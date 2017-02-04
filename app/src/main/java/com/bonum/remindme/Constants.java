31d02121a2e7bb0e310a747332dcbfd03d9142db
sheidon <ozaHIP78>
2017-02-04 07:07:44 +0700

    Implemented NavigationView(Drawer) and Tabs. Than set up test Activity.

---
+++ app/src/main/java/com/bonum/remindme/Constants.java
@@ -0,0 +1,13 @@
+package com.bonum.remindme;
+
+/**
+ * Created by user on 04.02.2017.
+ */
+
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+
+}
